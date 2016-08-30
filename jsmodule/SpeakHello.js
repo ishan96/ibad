d2333bde369409826735d11f759a1ac57a2a59c1
ishan96 <ishan2796@gmail.com>
2016-08-30 23:17:42 +0530

    java

--- jsmodule/SpeakHello.js
+++ jsmodule/SpeakHello.js
@@ -1,14 +1,11 @@
-
-(function(window) { 
-
-	var helloSpeaker = {};
-	helloSpeaker.name=helloSpeaker.speak;
-    
-	var speakWord = "Hello ";
-
-
-helloSpeaker.speak=function () {
-  console.log(speakWord + " " + names[i]);
-}
-window.helloSpeaker=helloSpeaker;
+(function(window)
+{
+  var helloSpeaker = {};
+  helloSpeaker.name = helloSpeaker.speak;
+  var speakWord = "Hello ";
+  helloSpeaker.speak = function()
+  {
+    console.log(speakWord + " " + names[i]);
+  }
+  window.helloSpeaker = helloSpeaker;
 })(window);
